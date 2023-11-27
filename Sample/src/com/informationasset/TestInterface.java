91729a91d3d65336b4f5c5c94ba5f4dd6c740223
Akshay123-456 <akshaydeshmukh006@gmail.com>
2023-11-27 09:51:37 +0530

    Minor updates in Interface

--- Sample/src/com/informationasset/TestInterface.java
+++ Sample/src/com/informationasset/TestInterface.java
@@ -3,12 +3,14 @@ package com.informationasset;
 public interface TestInterface {
 void show();
 String message();
-private String getMessage()
+private String getMessage(String msg)
 {
-return message();	
+int statusCode=getCode(10);
+return msg+"\n"+statusCode;	
 }
   private int getCode(int statusCode)
   {
+	  getMessage("Error while execution");
     return statusCode;
   }
 }
